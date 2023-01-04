df0c6528a4ac9dd6f17230c14eee2e2850f91a40
Jorge Aguilar <jorge.aguilar@gluo.mx>
2023-01-04 13:07:07 -0600

    First Soap service, error at response

---
+++ soap-service-homework/src/main/java/com/example/soapservicehomework/SoapServiceHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.soapservicehomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapServiceHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapServiceHomeworkApplication.class, args);
+	}
+
+}
---
+++ soap-service-homework/src/test/java/com/example/soapservicehomework/SoapServiceHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soapservicehomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServiceHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
