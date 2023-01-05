2da5c9bb3a329101c9b5e8d3bd3cdb4346fac3c2
Jorge Aguilar <jorge.aguilar@gluo.mx>
2023-01-04 20:11:37 -0600

    Service to consume countries

---
+++ countries-consume-service-soap/src/main/java/com/example/countriesconsumeservicesoap/CountryClient.java
@@ -0,0 +1,22 @@
+package com.example.countriesconsumeservicesoap;
+
+import com.example.countriesconsumeservicesoap.wsdl.GetCountryRequest;
+import com.example.countriesconsumeservicesoap.wsdl.GetCountryResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+public class CountryClient extends WebServiceGatewaySupport {
+    private static final Logger log = LoggerFactory.getLogger(CountryClient.class);
+    public GetCountryResponse getCountry(String country) {
+        GetCountryRequest request = new GetCountryRequest();
+        request.setName(country);
+        log.info("Requesting location for " + country);
+        GetCountryResponse response = (GetCountryResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8080/ws/countries", request,
+                        new SoapActionCallback("http://www.baeldung.com/springsoap/gen"));
+
+        return response;
+    }
+}
---
+++ countries-consume-service-soap/src/test/java/com/example/countriesconsumeservicesoap/CountriesConsumeServiceSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countriesconsumeservicesoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesConsumeServiceSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
